3b1c55e5b610610d37fb062748567bccd8bbc31e
Sabrina Li <sabrinali0312@gmail.com>
2020-02-11 17:08:00 -0500

    fruit list view with dummy data

--- app/src/main/java/com/example/helloworld_java/MainActivity.java
+++ app/src/main/java/com/example/helloworld_java/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.helloworld_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+import com.example.helloworld_java.data.FruitListData;
+
+public class MainActivity extends AppCompatActivity implements FruitRecyclerViewAdapter.FruitAdapterOnClickHandler {
+
+    private RecyclerView mRecyclerView;
+    private FruitRecyclerViewAdapter mFruitRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_fruit);
+
+        LinearLayoutManager layoutManager
+                =new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mFruitRecyclerViewAdapter = new FruitRecyclerViewAdapter(this);
+        mRecyclerView.setAdapter(mFruitRecyclerViewAdapter);
+
+        showFruitList();
+    }
+
+    private void showFruitList(){
+        String[] fruitList = FruitListData.getFruitlist();
+        mFruitRecyclerViewAdapter.setFruitList(fruitList);
+
+    }
+
+    @Override
+    public void onClick(String fruitName){
+        Toast.makeText(this,fruitName,Toast.LENGTH_SHORT)
+                .show();
     }
 }
