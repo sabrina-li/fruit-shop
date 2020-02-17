7eae8fefc050f34bb38a9a0986831a69dde7ea0d
Sabrina Li <sabrinali0312@gmail.com>
2020-02-17 11:51:02 -0500

    add back attachBaseContext override to final to App.java

--- app/src/main/java/com/example/helloworld_java/App.java
+++ app/src/main/java/com/example/helloworld_java/App.java
@@ -8,8 +8,8 @@ public class App extends MultiDexApplication {
     /**
      * This method just exists here to override as final so that we can be sure we handle this case.
      */
-//    @Override
-//    protected final void attachBaseContext(Context base) {
-//        super.attachBaseContext(base);
-//    }
+    @Override
+    protected final void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
 }
