e2792da5723b230d01b39c48bde2717f7f731ff5
Sabrina Li <sabrinali0312@gmail.com>
2020-02-19 13:45:33 -0500

    add product Dao to app database

--- app/src/main/java/com/example/helloworld_java/data/AppDatabase.java
+++ app/src/main/java/com/example/helloworld_java/data/AppDatabase.java
@@ -5,5 +5,5 @@ import androidx.room.RoomDatabase;
 
 //@Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
-//    public abstract UserDao userDao();
+    public abstract ProductDao productDao();
 }
