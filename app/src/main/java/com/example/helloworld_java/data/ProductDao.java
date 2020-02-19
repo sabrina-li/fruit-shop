e7fb2346c0215accf4f340622ec4f7934244f8db
Sabrina Li <sabrinali0312@gmail.com>
2020-02-19 13:38:07 -0500

    add product dao
    add product dao

---
+++ app/src/main/java/com/example/helloworld_java/data/ProductDao.java
@@ -0,0 +1,24 @@
+package com.example.helloworld_java.data;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ProductDao {
+    @Query("SELECT * FROM product")
+    List<Product> getAll();
+
+    @Query("SELECT * FROM product WHERE title IN (:productTitles)")
+    List<Product> loadAllByTitles(String[] productTitles);
+
+
+    @Insert
+    void insertAll(Product... products);
+
+    @Delete
+    void delete(Product product);
+}
